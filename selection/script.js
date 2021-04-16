821adcf7e08d50cf3030d0b6f362185f1d5a5c26
yUm1k4 <zaiabdullah99@gmail.com>
2021-04-16 10:09:53 +0700

    #4 dom selection part 2

--- selection/script.js
+++ selection/script.js
@@ -17,3 +17,33 @@ p[2].style.backgroundColor = 'lightblue';
 // document.getElementsByClassName() -> HTMLCollections
 const p1 = document.getElementsByClassName('p1');
 p1[0].innerHTML = 'Ini diubah dari js'; 
+
+
+/*
+* Dom Selection part 2
+*/
+
+// document.querySelector() -> menghasilkan elemen
+const p4 = document.querySelector('#b p');
+p4.style.color = 'green';
+p4.style.fontSize = '30px';
+
+const li2 = document.querySelector('section#b ul li:nth-child(2)')
+li2.style.backgroundColor = 'orange';
+
+// document.querySelectorAll -> menghasilkan nodeList
+// sama seperti getElementsByClassName
+const pAll = document.querySelectorAll('p');
+// pAll[1].style.backgroundColor = 'lightblue';
+// jika ingin semua:
+for (let i = 0; i < pAll.length; i++) {
+    pAll[i].style.backgroundColor = 'red';
+}
+
+
+/*
+* Mengubah Node Root
+*/
+const sectionB = document.getElementById('b');
+const paragraf4 = sectionB.querySelector('p');
+paragraf4.style.backgroundColor = 'cyan';
